469dac3ffd7913a40c0447a5f7da0ea273945214
zh0ngtian <zhongtian1994@qq.com>
2023-12-30 15:58:18 +0800

    fix direct_conv

--- direct_conv.cu
+++ direct_conv.cu
@@ -29,7 +29,14 @@ void cpu_conv(float *in, float *out, float *cpu_kernel, int in_c, int in_h, int 
     }
 }
 
-template <const int BLOCK_HEIGHT, const int BLOCK_WIDTH, const int KERNEL_HEIGHT, const int KERNEL_WIDTH, const int MALLOC_TEMP_SIZE, const int MALLOC_BLOCK_HEIGHT, const int MALLOC_BLOCL_WIDTH>
+template <const int BLOCK_HEIGHT,
+          const int BLOCK_WIDTH,
+          const int KERNEL_HEIGHT,
+          const int KERNEL_WIDTH,
+          const int MALLOC_TEMP_SIZE,
+          const int MALLOC_BLOCK_HEIGHT,
+          const int MALLOC_BLOCL_WIDTH,
+          const int OUTPUT_PER_THREAD>
 __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, int in_h, int in_w, int out_c, int out_h, int out_w) {
     int block_y = blockIdx.y;
     int block_x = blockIdx.x;
@@ -92,10 +99,10 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
 
     // copy kernel data
     if (thread_x == 0) {
-        for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; i++) {
+        for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; ++i) {
             int yy = i * BLOCK_HEIGHT + thread_y;
             if (yy >= KERNEL_HEIGHT) break;
-            for (int j = 0; j < KERNEL_WIDTH; j++) {
+            for (int j = 0; j < KERNEL_WIDTH; ++j) {
                 s_kernel[yy][j] = kernel[OFFSET(yy, j, KERNEL_WIDTH)];
             }
         }
@@ -104,13 +111,13 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
     __syncthreads();
 
     // 以下变量都是针对输出矩阵而言
-    int cur_out_block_height = BLOCK_HEIGHT;  // 输出 block 的尺寸
-    int cur_out_block_width = BLOCK_WIDTH;    // 输出 block 的尺寸
-    int single_calculate_num = 2;             // 每个线程负责计算的输出元素数目
-    int out_tile_thread_per_row;              // 输出 block 每行需要的线程数
-    int out_tile_row_start;                   // tile 在 sub-block 中的行坐标
-    int out_tile_col;                         // tile 在 sub-block 中的列坐标
-    int out_tile_row_stride;                  // 同一个线程两次计算需要跨越的行数（同一个 block 中 sub-block 之间相隔的行数）
+    int cur_out_block_height = BLOCK_HEIGHT;       // 输出 block 的尺寸
+    int cur_out_block_width = BLOCK_WIDTH;         // 输出 block 的尺寸
+    int single_calculate_num = OUTPUT_PER_THREAD;  // 每个线程负责计算的输出元素数目
+    int out_tile_thread_per_row;                   // 输出 block 每行需要的线程数
+    int out_tile_row_start;                        // tile 在 sub-block 中的行坐标
+    int out_tile_col;                              // tile 在 sub-block 中的列坐标
+    int out_tile_row_stride;                       // 同一个线程两次计算需要跨越的行数（同一个 block 中 sub-block 之间相隔的行数）
 
     // 修正边缘 block 尺寸
     if (block_y == boundary_y) {
@@ -127,21 +134,21 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
 
     float val[MALLOC_TEMP_SIZE];
 
-    for (int oc = 0; oc < out_c; oc++) {
+    for (int oc = 0; oc < out_c; ++oc) {
         for (int i = 0; i < MALLOC_TEMP_SIZE; ++i) val[i] = 0;
 
-        for (int ic = 0; ic < in_c; ic++) {
+        for (int ic = 0; ic < in_c; ++ic) {
             for (int i = 0; i < cur_out_block_height && (out_tile_row_start + i) < cur_out_block_height; i += out_tile_row_stride) {
                 // 如果 block 的宽度不能被 tile 宽度整除，对于 block 中的边缘 tile 做特殊处理
                 int new_single_calculate_num = single_calculate_num;
                 if (out_tile_col + 2 * single_calculate_num > cur_out_block_width && out_tile_col + 1 * single_calculate_num < cur_out_block_width) {
-                    new_single_calculate_num = 3;  // TODO: do not use constant 3
+                    new_single_calculate_num = cur_out_block_width - out_tile_col;  // 对于 block 中的边缘 tile，每个线程负责计算的输出元素个数也会多一点
                 }
 
-                for (int j = 0; j < new_single_calculate_num; j++) {
+                for (int j = 0; j < new_single_calculate_num; ++j) {
                     int temp_pos = i / out_tile_row_stride * new_single_calculate_num + j;
-                    for (int ii = 0; ii < KERNEL_HEIGHT; ii++) {
-                        for (int jj = 0; jj < KERNEL_WIDTH; jj++) {
+                    for (int ii = 0; ii < KERNEL_HEIGHT; ++ii) {
+                        for (int jj = 0; jj < KERNEL_WIDTH; ++jj) {
                             val[temp_pos] += s_in[out_tile_row_start + i + ii][out_tile_col + j + jj] * s_kernel[ii][jj];
                         }
                     }
@@ -150,23 +157,25 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
 
             // 预读取下一个 in channel 和对应 kernel in channel
             if (ic < in_c - 1) {
-                for (int i = 0; i < cur_in_block_height && in_tile_row_start < cur_in_block_height; i += in_tile_row_stride) {
-                    FETCH_FLOAT4(load_reg[0]) = FETCH_FLOAT4(in[begin_pos + (ic + 1) * in_h * in_w + OFFSET(in_tile_row_start + i, in_tile_col, in_w)]);
-                    s_in[in_tile_row_start + i][in_tile_col + 0] = load_reg[0];
-                    s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
-                    s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
-                    s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
-                    if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width && cur_in_block_width > in_tile_col + 1 * single_trans_ele_num) {
-                        for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; j++) {
-                            s_in[in_tile_row_start + i][j] = in[begin_pos + (ic + 1) * in_h * in_w + OFFSET(in_tile_row_start + i, j, in_w)];
+                if (in_tile_row_start < cur_in_block_height) {
+                    for (int i = 0; i < cur_in_block_height; i += in_tile_row_stride) {
+                        FETCH_FLOAT4(load_reg[0]) = FETCH_FLOAT4(in[begin_pos + (ic + 1) * in_h * in_w + OFFSET(in_tile_row_start + i, in_tile_col, in_w)]);
+                        s_in[in_tile_row_start + i][in_tile_col + 0] = load_reg[0];
+                        s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
+                        s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
+                        s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
+                        if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width && in_tile_col + 1 * single_trans_ele_num < cur_in_block_width) {
+                            for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; ++j) {
+                                s_in[in_tile_row_start + i][j] = in[begin_pos + (ic + 1) * in_h * in_w + OFFSET(in_tile_row_start + i, j, in_w)];
+                            }
                         }
                     }
                 }
                 if (thread_x == 0) {
-                    for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; i++) {
+                    for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; ++i) {
                         int yy = i * BLOCK_HEIGHT + thread_y;
                         if (yy >= KERNEL_HEIGHT) break;
-                        for (int j = 0; j < KERNEL_WIDTH; j++) {
+                        for (int j = 0; j < KERNEL_WIDTH; ++j) {
                             s_kernel[yy][j] = kernel[(oc * in_c + ic + 1) * KERNEL_HEIGHT * KERNEL_WIDTH + OFFSET(yy, j, KERNEL_WIDTH)];
                         }
                     }
@@ -179,10 +188,10 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
         // 预读取下一个 kernel out channel
         if (oc + 1 < out_c) {
             if (thread_x == 0) {
-                for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; i++) {
+                for (int i = 0; i < KERNEL_HEIGHT / BLOCK_HEIGHT + 1; ++i) {
                     int yy = i * BLOCK_HEIGHT + thread_y;
                     if (yy >= KERNEL_HEIGHT) break;
-                    for (int j = 0; j < KERNEL_WIDTH; j++) {
+                    for (int j = 0; j < KERNEL_WIDTH; ++j) {
                         s_kernel[yy][j] = kernel[(oc + 1) * in_c * KERNEL_HEIGHT * KERNEL_WIDTH + OFFSET(yy, j, KERNEL_WIDTH)];
                     }
                 }
@@ -192,35 +201,32 @@ __global__ void gpu_direct_conv(float *in, float *out, float *kernel, int in_c, 
         __syncthreads();
 
         // 写回
-        int i = 0, j = 0;
-        while (i < cur_out_block_height && (out_tile_row_start + i) < cur_out_block_height) {
+        for (int i = 0; i < cur_out_block_height && (out_tile_row_start + i) < cur_out_block_height; i += out_tile_row_stride) {
             // 如果 block 的宽度不能被 tile 宽度整除，对于 block 中的边缘 tile 做特殊处理
             int new_single_calculate_num = single_calculate_num;
             if (out_tile_col + 2 * single_calculate_num > cur_out_block_width && out_tile_col + 1 * single_calculate_num < cur_out_block_width) {
-                new_single_calculate_num = 3;  // TODO: do not use constant 3
+                new_single_calculate_num = cur_out_block_width - out_tile_col;
             }
 
-            while (j < new_single_calculate_num) {
+            for (int j = 0; j < new_single_calculate_num; ++j) {
                 int out_pos = oc * out_h * out_w + block_y * BLOCK_HEIGHT * out_w + block_x * BLOCK_WIDTH + OFFSET(out_tile_row_start + i, out_tile_col + j, out_w);
                 int temp_pos = i / out_tile_row_stride * new_single_calculate_num + j;
                 out[out_pos] = val[temp_pos];
-                j++;
             }
-
-            i += out_tile_row_stride;
-            j = 0;
         }
 
         // 预读取下一个 in channel
-        for (int i = 0; i < cur_in_block_height && in_tile_row_start < cur_in_block_height; i += in_tile_row_stride) {
-            FETCH_FLOAT4(load_reg[0]) = FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, in_w)]);
-            s_in[in_tile_row_start + i][in_tile_col + 0] = load_reg[0];
-            s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
-            s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
-            s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
-            if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width && cur_in_block_width > in_tile_col + 1 * single_trans_ele_num) {
-                for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; j++) {
-                    s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, in_w)];
+        if (in_tile_row_start < cur_in_block_height) {  // 超出当前 block 范围的线程不做拷贝操作
+            for (int i = 0; i < cur_in_block_height; i += in_tile_row_stride) {
+                FETCH_FLOAT4(load_reg[0]) = FETCH_FLOAT4(in[begin_pos + OFFSET(in_tile_row_start + i, in_tile_col, in_w)]);
+                s_in[in_tile_row_start + i][in_tile_col + 0] = load_reg[0];
+                s_in[in_tile_row_start + i][in_tile_col + 1] = load_reg[1];
+                s_in[in_tile_row_start + i][in_tile_col + 2] = load_reg[2];
+                s_in[in_tile_row_start + i][in_tile_col + 3] = load_reg[3];
+                if (in_tile_col + 2 * single_trans_ele_num > cur_in_block_width && in_tile_col + 1 * single_trans_ele_num < cur_in_block_width) {
+                    for (int j = in_tile_col + 1 * single_trans_ele_num; j < cur_in_block_width; ++j) {
+                        s_in[in_tile_row_start + i][j] = in[begin_pos + OFFSET(in_tile_row_start + i, j, in_w)];
+                    }
                 }
             }
         }
@@ -247,7 +253,7 @@ int main() {
     cpu_output = (float *)malloc(output_size * sizeof(float));
     cpu_kernel = (float *)malloc(kernel_size * sizeof(float));
     cuda_output = (float *)malloc(output_size * sizeof(float));
-    for (int i = 0; i < input_size; i++) {
+    for (int i = 0; i < input_size; ++i) {
         cpu_input[i] = i;
     }
     for (int i = 0; i < kernel_size; ++i) {
@@ -281,9 +287,10 @@ int main() {
         printf("cudaMemcpy kernel failed: %s\n", cudaGetErrorString(err));
     }
 
-    const int MALLOC_BLOCK_HEIGHT = (BLOCK_HEIGHT + KERNEL_HEIGHT) * 2;  // double buffer
-    const int MALLOC_BLOCK_WIDTH = (BLOCK_WIDTH + KERNEL_WIDTH) * 2;     // double buffer
-    const int MALLOC_TEMP_SIZE = out_c * 8;                              // temp out channel data, TODO
+    const int MALLOC_BLOCK_HEIGHT = (BLOCK_HEIGHT + KERNEL_HEIGHT) * 2;  // 乘以 2 为了应对边缘 block 的情况
+    const int MALLOC_BLOCK_WIDTH = (BLOCK_WIDTH + KERNEL_WIDTH) * 2;     // 乘以 2 为了应对边缘 block 的情况
+    const int OUTPUT_PER_THREAD = 2;
+    const int MALLOC_TEMP_SIZE = out_c * 8;  // temp out channel data
 
     printf("KERNEL_HEIGHT: %d, KERNEL_WIDTH: %d, MALLOC_BLOCK_HEIGHT: %d, MALLOC_BLOCK_WIDTH: %d, MALLOC_TEMP_SIZE: %d\n",
            KERNEL_HEIGHT,
@@ -295,7 +302,7 @@ int main() {
     dim3 dim_grid(out_w / BLOCK_WIDTH, out_h / BLOCK_HEIGHT);
     dim3 dim_block(BLOCK_WIDTH, BLOCK_HEIGHT);
 
-    gpu_direct_conv<BLOCK_HEIGHT, BLOCK_WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, MALLOC_TEMP_SIZE, MALLOC_BLOCK_HEIGHT, MALLOC_BLOCK_WIDTH>
+    gpu_direct_conv<BLOCK_HEIGHT, BLOCK_WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, MALLOC_TEMP_SIZE, MALLOC_BLOCK_HEIGHT, MALLOC_BLOCK_WIDTH, OUTPUT_PER_THREAD>
         <<<dim_grid, dim_block>>>(gpu_input, gpu_output, gpu_kernel, in_c, in_h, in_w, out_c, out_h, out_w);
     err = cudaGetLastError();
     if (err != cudaSuccess) {
@@ -308,7 +315,7 @@ int main() {
         printf("cudaMemcpy output failed: %s\n", cudaGetErrorString(err));
     }
 
-    for (int i = 0; i < output_size; i++) {
+    for (int i = 0; i < output_size; ++i) {
         if (cpu_output[i] != cuda_output[i]) {
             printf("WRONG VALUE: %.2f | %.2f at %d\n", cpu_output[i], cuda_output[i], i);
             break;
